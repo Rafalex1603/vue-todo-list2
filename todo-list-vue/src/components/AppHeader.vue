cfcc065de652fed8a30bd77d1bb6434779b51a93
unknown <rafalexwtf@gmail.com>
2024-12-14 17:15:28 -0400

    header componetn done

---
+++ todo-list-vue/src/components/AppHeader.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <h1>
+            <span class="icon" aria-hidden="true">to do</span>
+        </h1>
+        <p>lista de tareas en Vue.JS 3</p>
+    </header>
+</template>
+
+<style scoped>
+    header{
+        border-bottom: #333 1xp solid;
+        background-color: #fff;
+    }
+    header{
+        content: "";
+        display: block;
+        height: 1px;
+        box-sizing: 0px 0px 10px 0px rgba(0,0,0,0.75);
+    }
+    h1{
+        font-size: 2rem;
+    }
+    h1 .icon {
+        font-size: 1rem;
+        vertical-align: middle;
+    }
+</style>
